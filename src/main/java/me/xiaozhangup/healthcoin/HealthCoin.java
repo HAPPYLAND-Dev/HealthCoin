2420eccee12a1a2e9d774b02685d60d6cec718e3
xiaozhangup <xiaozhangshiw@163.com>
2022-07-16 22:45:58 +0800

    Initial commit

---
+++ src/main/java/me/xiaozhangup/healthcoin/HealthCoin.java
@@ -0,0 +1,11 @@
+package me.xiaozhangup.healthcoin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HealthCoin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("HealthCoin Loaded!");
+    }
+}
